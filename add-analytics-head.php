da507263523bf3ce13ea659adfe097a328b53264
13cweb <info@13cweb.com>
2026-01-05 15:17:28 +0900

    Initial commit: My awesome theme

---
+++ add-analytics-head.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Luxeritas WordPress Theme - free/libre wordpress platform
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * @copyright Copyright (C) 2015 Thought is free.
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
+ * @author LunaNuko
+ * @link https://thk.kanzae.net/
+ * @translators rakeem( http://rakeem.jp/ )
+ */
+
+/* Google Analytics(gtag.js) 等、アクセス解析の Javascript は、?> 以降に書いてください。
+ (Javascript of Access analysis (Google Analytics gtag.js etc.), please write them after the below ?>. ) */
+?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-88128601-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-88128601-1');
+</script>
+
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-1865972142580493",
+    enable_page_level_ads: true
+  });
+</script>
