a7428f1bb61d88bf2b673705ac62cf7cdc2a9ed4
13cweb <info@13cweb.com>
2026-01-09 12:53:41 +0900

    Markdownで自サイト以外のリンクはblank

--- add-footer.php
+++ add-footer.php
@@ -2,3 +2,11 @@
 /* フッターに CSS や Javascript 等を追加したい場合は、?> 以降に書いてください。
  (To add CSS or Javascript in the footer, please write them after the below ?>. ) */
 ?>
+<script>
+document.addEventListener("DOMContentLoaded", function () {
+    var links = document.querySelectorAll("a[href^='http']:not([href*='" + window.location.hostname + "'])");
+    links.forEach(function (link) {
+        link.setAttribute('target', '_new');
+    });
+});
+</script>
